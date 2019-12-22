2b13663fdf6132fbf05ef386c8a61d67d24f4c50
Boros Tibor <boros2222@gmail.com>
2019-12-22 13:04:57 +0100

    Changes to gitignore

--- backend/btwsbe/src/main/java/eu/borostack/annotation/CheckUserId.java
+++ backend/btwsbe/src/main/java/eu/borostack/annotation/CheckUserId.java
@@ -1,13 +1,11 @@
 package eu.borostack.annotation;
 
-import javax.interceptor.InterceptorBinding;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@InterceptorBinding
 @Target({PARAMETER})
 @Retention(RUNTIME)
 public @interface CheckUserId {
